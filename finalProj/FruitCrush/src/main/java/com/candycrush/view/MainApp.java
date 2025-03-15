5f0246ad4c2a0db6585591f4c29fa6340f421aaf
Jingjing Pan <pan_jingjing@foxmail.com>
2025-03-14 22:46:22 -0700

    Gold painted

--- finalProj/FruitCrush/src/main/java/com/candycrush/view/MainApp.java
+++ finalProj/FruitCrush/src/main/java/com/candycrush/view/MainApp.java
@@ -7,7 +7,9 @@ public class MainApp extends JFrame {
   public static void main(String[] args) {
     JFrame frame = new JFrame("Fruit Crush");
     frame.setSize(600, 600);
+    frame.setLocationRelativeTo(null);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 
     StartPanel startPanel = new StartPanel(frame);
     frame.add(startPanel);
